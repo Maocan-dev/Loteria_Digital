d8cba7412c30e5be91d522c16533714debab49ba
Maocan <manuel.8.andazola@gmail.com>
2025-04-18 11:23:55 -0600

    se creo app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.loteriadigital.app',
+  appName: 'loteria_digital',
+  webDir: 'dist'
+};
+
+export default config;
